8885a41b7ca7499736de25edf29f234a03540d5e
meishuen <fongmeishuen@gmail.com>
2022-11-05 21:51:09 +0800

    example

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native'; 
+import Navigator from './src/navigation/Navigator';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return(
+      <View style={styles.container}>  
+        <Navigator/>
+      </View>
+    )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1, 
   },
 });
